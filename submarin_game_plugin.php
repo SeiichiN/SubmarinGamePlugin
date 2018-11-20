28aa71973078abdc0ee967d6a92a66eb68390ab2
vagrant <vagrant@vagrant.vm>
2018-11-20 06:26:29 +0000

    douyaroka/

--- submarin_game_plugin.php
+++ submarin_game_plugin.php
@@ -8,8 +8,8 @@
  */
 
 function add_files() {
-	wp_enqueue_script('submarin.js', plugins_url('submarin.js'));
-	wp_register_style('submarin.css', plugins_url('submarin.css'));
+	wp_enqueue_script('submarin.js', plugins_url('submarin.js', __FILE__), array(), '1.0', true);
+	wp_register_style('submarin.css', plugins_url('submarin.css', __FILE__));
     wp_enqueue_styles('submarin.css');
 }
 add_action('wp_enqueue_scripts', 'add_files');
